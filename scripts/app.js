3a3f583f8a720a1fc94387cf31d3d141f35238c1
Mateusz_Prokop <mateusz.prokopp@gmail.com>
2021-08-03 17:52:45 +0200

    Added change username feature

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,29 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 
 //add a new chat
 newChatForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const message = newChatForm.message.value.trim();
-	chatroom.addChat(message)
-		.then(() => newChatForm.reset())
-		.catch((err) => console.log(err));
+	chatroom.addChat(message).then(() => newChatForm.reset()).catch((err) => console.log(err));
+});
+
+// update username
+newNameForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	//update name in chatroom
+	const newName = newNameForm.name.value.trim();
+	chatroom.updateName(newName);
+	//reset the form
+	newNameForm.reset();
+	//show then hide the update msg
+	updateMssg.innerText = `Your name was updated to ${newName}`;
+	setTimeout(() => {
+		updateMssg.innerText = '';
+	}, 3000);
 });
 
 //class instances
